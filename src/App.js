8cafd2ba13d0c0ca1eda3763d0a83b5bcbbe20ab
affan <affan@eatlbd.com>
2022-04-10 12:24:51 +0600

    i

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import Home from "./pages/Home";
+import React, { Suspense } from 'react';
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import "animate.css";
 import NavBar from "./views/NavBar";
 import AppFooter from "./views/AppFooter";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import HomeOne from "./pages/HomeOne";
-import AllProjects from "./pages/AllProjects";
-import AllProducts from "./pages/AllProducts";
-import LatestUpdate from "./pages/LatestUpdate";
-import DirectorsMessage from "./views/DirectorsMessage";
-import NotFound from "./views/NotFound";
-import NewsDetails from "./views/NewsDetails";
+import "animate.css";
+// import Home from "./pages/Home";
+import Spinner from './components/Spinner';
+const Home = React.lazy(() => import("./pages/Home"));
+const About = React.lazy(() => import("./pages/About"));
+const DirectorsMessage = React.lazy(() => import("./views/DirectorsMessage"));
+const AllProducts = React.lazy(() => import("./pages/AllProducts"));
+const AllProjects = React.lazy(() => import("./pages/AllProjects"));
+const LatestUpdate = React.lazy(() => import("./pages/LatestUpdate"));
+const NewsDetails = React.lazy(() => import("./views/NewsDetails"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const NotFound = React.lazy(() => import("./views/NotFound"));
 
 AOS.init();
 function App() {
   return (
-    <div className="App custombg1">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/1" element={<HomeOne />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/directors-message" element={<DirectorsMessage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<AllProjects />} />
-        <Route path="/products" element={<AllProducts />} />
-        <Route path="/latest-update" element={<LatestUpdate />} />
-        <Route path="/news-details" element={<NewsDetails />} />
-        <Route
-          path="*"
-          element={<NotFound />}
-        />
-      </Routes>
-      <AppFooter />
-    </div>
+    <Suspense fallback={<Spinner/>}>
+      <div className="App custombg1">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/directors-message" element={<DirectorsMessage />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<AllProjects />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/latest-update" element={<LatestUpdate />} />
+          <Route path="/news-details" element={<NewsDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <AppFooter />
+      </div>
+    </Suspense>
   );
 }
 
